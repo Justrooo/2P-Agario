6579abac6441ec82a81295e8d5e0365f8266d0d7
Justrooo <87012050+Justrooo@users.noreply.github.com>
2021-08-29 16:55:24 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -11,9 +11,8 @@ pygame.init()
 clock = pygame.time.Clock()
 
 # Screen -------------------------------------------------------------------
-sc = pygame.display.Info()  # info class object
-w, h = 1200, 800
-screen = pygame.display.set_mode((sc.current_w, sc.current_h-55))
+w, h = 1920, 1080
+screen = pygame.display.set_mode((w, h-55))
 pygame.display.set_caption("Agario but bad")
 bg = ui_scripts.ui_bg.Background("Images/background.png")
 menu_bg = ui_scripts.ui_bg.Background("Images/UI/background.png")
@@ -33,35 +32,33 @@ pr_group = pygame.sprite.Group()
 pr_group.add(pr)
 pr_group.add(pr2)
 
-if texture == texture2:
-    if pr.image != player.player_textures[5]:
-        pr2.image = pygame.image.load(player.player_textures[5])
-        for x in range(3):  # Animation bc why not :D
-            if x == 0: print("[class: Player] player1's texture was same as player2's texture. REMOVING.")
-            elif x == 1: print("[class: Player] player1's texture was same as player2's texture. REMOVING..")
-            else: print("[class: Player] player1's texture was same as player2's texture. REMOVING...")
+while texture == texture2:
+    texture2 = pygame.image.load(choice(player.player_textures))
+    pr2.image = texture2
+    for x in range(3):  # Animation bc why not :D
+        if x == 0: print("[class: Player] player1's texture was same as player2's texture. REMOVING.")
+        elif x == 1: print("[class: Player] player1's texture was same as player2's texture. REMOVING..")
+        else: print("[class: Player] player1's texture was same as player2's texture. REMOVING...")
 
-    else:
-        print("ELSE")
-        pr2.image = pygame.image.load(player.player_textures[4])
-else: pass
 # Players ------------------------------------------------------------------
-# Fonts & Texts ------------------------------------------------------------
-input_font = pygame.font.Font("Fonts/input_font.ttf", 32)
-button_font = pygame.font.Font("Fonts/button_font.ttf", 78)
-# pr.nick = ui_scripts.ui_text.Text("Fonts/button_font.ttf", 78)  # FINISH LATER
-
-# Fonts & Texts ------------------------------------------------------------
 # Panels -------------------------------------------------------------------
 # Left panel
 panel_left = ui_scripts.ui_panel.Panel("Images/UI/box.png", 500, 600)
 # Right panel
-panel_right = ui_scripts.ui_panel.Panel("Images/UI/box.png", sc.current_w-500, 600)
+panel_right = ui_scripts.ui_panel.Panel("Images/UI/box.png", w-500, 600)
 # Group
 panel_group = pygame.sprite.Group()
 panel_group.add(panel_left)
 panel_group.add(panel_right)
 # Panels -------------------------------------------------------------------
+# Fonts & Texts ------------------------------------------------------------
+input_font = pygame.font.Font("Fonts/input_font.ttf", 32)
+button_font = pygame.font.Font("Fonts/button_font.ttf", 78)
+
+p1text = ui_scripts.ui_text.Text("Fonts/button_font.ttf", 78, pr.nick, panel_left.rect.centerx,
+                                 panel_left.rect.centery)
+p2text = ui_scripts.ui_text.Text("Fonts/button_font.ttf", 78, pr2.nick, 0, 0)
+# Fonts & Texts ------------------------------------------------------------
 # InputBox -----------------------------------------------------------------
 inputBox = textbox.InputBox(input_font, "Images/UI/inputbox.png", panel_left.rect.centerx,
                             panel_left.image.get_height()-370)
@@ -84,6 +81,7 @@ while True:
                         inputBox.uinput = inputBox.uinput[:-1]
                     else:
                         inputBox.uinput += event.unicode
+
             if event.type == pygame.MOUSEBUTTONDOWN:
                 if inputBox.rect.collidepoint(event.pos):
                     inputBox.active = True
@@ -97,6 +95,8 @@ while True:
         input_group.draw(screen)  # InputBox
 
         screen.blit(inputBox.update_text(), (inputBox.rect.x+5, inputBox.rect.y))  # Input Text
+        screen.blit(p1text.update_text(), p1text.rect.center)  # P1 nick update
+        screen.blit(p2text.update_text(), (100, 100))  # P2 nick update
 
         if len(inputBox.uinput) > inputBox.char:
             inputBox.uinput = inputBox.uinput[:inputBox.char]
@@ -104,8 +104,8 @@ while True:
             elif "mmmmmm" in inputBox.uinput: inputBox.char = 6
             else: inputBox.char = 10
 
-        button_text_surface = button_font.render(pr.nick, True, (64, 64, 64))  # Nick
-        screen.blit(button_text_surface, panel_left.rect.center)
+        # button_text_surface = button_font.render(pr.nick, True, (64, 64, 64))  # Nick
+        # screen.blit(button_text_surface, panel_left.rect.center)
 
     elif menu_state == 1:
         keys = pygame.key.get_pressed()
@@ -124,9 +124,9 @@ while True:
         # Movement ---------------------------------------------------------
         # Border
         if pr.rect.left <= 0: pr.rect.left = pr.rect.x + 10
-        if pr.rect.right >= sc.current_w: pr.rect.left = pr.rect.x - 10
+        if pr.rect.right >= w: pr.rect.left = pr.rect.x - 10
         if pr.rect.top <= 0: pr.rect.top = pr.rect.y + 10
-        if pr.rect.bottom >= sc.current_h: pr.rect.top = pr.rect.y - 10
+        if pr.rect.bottom >= h-50: pr.rect.top = pr.rect.y - 10
 
         # Draw background
         screen.blit(bg.image, (0, 0))
