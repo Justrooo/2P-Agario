93aceebe2740b449b72e8a71fe8bc850c29b47f7
Justrooo <87012050+Justrooo@users.noreply.github.com>
2021-08-29 16:57:29 +0200

    Add files via upload

--- ui_scripts/ui_text.py
+++ ui_scripts/ui_text.py
@@ -2,11 +2,13 @@ import pygame
 
 
 class Text(pygame.sprite.Sprite):
-    def __init__(self, font_path, font_res):
+    def __init__(self, font_path, font_res, text, x, y):
         super().__init__()
-        self.text = ""
+        self.text = text
         self.font = pygame.font.Font(font_path, font_res)
         self.text_surface = self.font.render("", True, 0)
+        self.rect = self.text_surface.get_rect()
+        self.rect.center = [x, y]
 
     def update_text(self):
         self.text_surface = self.font.render(self.text, True, (0, 0, 0))
